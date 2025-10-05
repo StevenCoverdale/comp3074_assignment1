6bbfdcc95bec7904bbbfc3d3b4bdbf7995f4fe08
David Rashidi <moewo@Davids-MacBook-Pro.local>
2025-10-05 17:54:14 -0400

    updated detail activity

--- app/src/main/java/com/example/comp3074_assignment1/DetailActivity.java
+++ app/src/main/java/com/example/comp3074_assignment1/DetailActivity.java
@@ -1,6 +1,27 @@
 package com.example.comp3074_assignment1;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        ListView list = findViewById(R.id.listPayments);
+        TextView empty = findViewById(R.id.emptyView);
+        list.setEmptyView(empty);
+
+        ArrayAdapter<Payment> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                PaymentRepo.PAYMENTS
+        );
+        list.setAdapter(adapter);
+    }
 }
